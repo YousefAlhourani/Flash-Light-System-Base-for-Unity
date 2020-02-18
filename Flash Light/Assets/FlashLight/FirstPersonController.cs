847d20d3664186c71ad23e134c7cadcb10a91937
YousefAlhourani <53283672+YousefAlhourani@users.noreply.github.com>
2020-02-18 20:52:38 +0200

    inital commit

---
+++ Flash Light/Assets/FlashLight/FirstPersonController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GabieStudio
+{
+    public class FirstPersonController : MonoBehaviour
+    {
+        [Tooltip("The Speed The Camera Moves")]
+        public float moveSpeed;
+        [SerializeField][Tooltip("Mouse Movement Speed")] float speedH;
+        float yaw = 0;
+        float yaw2 = 0;
+        private static readonly string _InputX = "Mouse X";
+        private static readonly string _InputY = "Mouse Y";
+        private static readonly string _MouseLeft = "Fire1"; //Set This To name in Input Manager (Left Mouse Click)
+        private static readonly string _MouseRight = "Fire2"; //Set this to name in Input Manager (Right Mouse Click)
+
+        
+        private void Update()
+        {
+            MoveFunction();
+        }
+        void MoveFunction()
+        {
+            float lr = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+            float fb = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+            transform.Translate(lr, 0, fb);
+            yaw += speedH * Input.GetAxis(_InputX);
+            yaw2 += speedH * Input.GetAxis(_InputY);
+            transform.eulerAngles = new Vector3(-yaw2, yaw, 0);
+        }
+    }
+
+   
+}
+
---
+++ Flash Light/Assets/FlashLight/FlashLightController.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GabieStudio
+{
+    [ExecuteInEditMode]
+    public class FlashLightController : MonoBehaviour
+    {
+        [Header("General Flash Light Settings\n")]
+        public FlashLightType flashLightType;
+        public float batteryPower;
+        public float batteryConsumptionRate;
+        private float _DefaultBatteryPower;
+        public enum FlashLightType { Infinite, BatteryPower }
+
+
+        //In Inspector Properties
+        [Header("Light Settings\n")]
+        [Range(1, 360)]
+        public float lightCircleAngle = 45f;
+        public float lightCastRange = 5f;
+        public float lightStrength = 1f;
+        public LightShadows shadowType = LightShadows.None;
+        public Color lightColor = Color.yellow;
+        public LightmapBakeType lightMode = LightmapBakeType.Realtime;
+        public Texture Cookie;
+        public Flare flare;
+        public LightRenderMode renderMode = LightRenderMode.Auto;
+        public bool drawHalo;
+
+
+
+        [Header("Light Referencing")]
+        public Light LightObject;
+
+        //Logic Related Data
+        private bool _ToggleLight { get; set; }
+        [HideInInspector] public bool GetLightStatus() { return _ToggleLight; }
+        [HideInInspector] public void SetLightStatus(bool X) { _ToggleLight = X; }
+
+        //singelton for referencing.
+        public static FlashLightController Instance;
+        private static readonly string _MouseLeft = "Fire1"; //Set This To name in Input Manager (Left Mouse Click)
+        private static readonly string _MouseRight = "Fire2"; //Set this to name in Input Manager (Right Mouse Click)
+
+        private void Awake() { Instance = this; }
+
+        private void Start()
+        {
+            if (!Application.isPlaying)
+            {
+                _ToggleLight = true;
+                LightObject.enabled = true;
+                LightObject.range = lightCastRange;
+                LightObject.shadows = shadowType;
+                LightObject.color = lightColor;
+                LightObject.lightmapBakeType = lightMode;
+                LightObject.intensity = lightStrength;
+                LightObject.spotAngle = lightCircleAngle;
+            }
+            else if (Application.isPlaying)
+            {
+                _DefaultBatteryPower = batteryPower;
+                _ToggleLight = false;
+                LightObject.enabled = false;
+                LightObject.range = lightCastRange;
+                LightObject.shadows = shadowType;
+                LightObject.color = lightColor;
+                LightObject.lightmapBakeType = lightMode;
+                LightObject.intensity = lightStrength;
+                LightObject.spotAngle = lightCircleAngle;
+            }
+        }
+       
+
+        private void Update()
+        {
+            if (!Application.isPlaying)
+            {
+
+                LightObject.enabled = true;
+                LightObject.range = lightCastRange;
+                LightObject.shadows = shadowType;
+                LightObject.color = lightColor;
+                LightObject.lightmapBakeType = lightMode;
+                LightObject.intensity = lightStrength;
+                LightObject.spotAngle = lightCircleAngle;
+            }
+
+            if (Application.isPlaying)
+            {
+
+                if(Input.GetButtonDown(_MouseLeft))
+                {
+                    if(GetLightStatus())
+                    {
+                        LightObject.enabled = false;
+                        SetLightStatus(false);
+                    }
+                    else
+                    if(!GetLightStatus())
+                    {
+                        LightObject.enabled = true;
+                        SetLightStatus(true);
+                    }
+                }
+
+                if(flashLightType==FlashLightType.BatteryPower)
+                {
+                    if(GetLightStatus())
+                    {
+                        batteryPower -= batteryConsumptionRate;
+                        if(batteryPower<=0)
+                        {
+                            SetLightStatus(false);
+                            LightObject.enabled = false;
+                        }
+                    }
+                    
+                    if(Input.GetButtonDown(_MouseRight))
+                    {
+                        batteryPower = _DefaultBatteryPower;
+                        //Reload Logic Place Here 
+                        //Todo:Connecting UI element to Display battery percentage.
+                    }
+                }
+
+            }
+        }
+    }
+}
